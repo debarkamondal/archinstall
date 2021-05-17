ebb34639b81d2a1334f2f527d0ccded266601a6f
Debarka Mondal <37607719+debarkamondal@users.noreply.github.com>
2021-05-17 13:39:51 +0530

    Create kde.sh

---
+++ kde.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+git clone https://aur.archlinux.org/paru.git
+cd paru/
+makepkg -si --noconfirm
+
+paru -S --noconfirm zramd
+sudo
+
+sudo pacman -S --noconfirm xorg sddm plasma firefox vlc alacritty
+
+sudo flatpak install -y spotify
+
+sudo systemctl enable sddm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+reboot
