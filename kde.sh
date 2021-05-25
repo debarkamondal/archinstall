40e4da7ed23085f44df7e1edf6b4cd886bdbfbcb
Debarka Mondal <37607719+debarkamondal@users.noreply.github.com>
2021-05-25 16:13:56 +0530

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -14,7 +14,7 @@ makepkg -si --noconfirm
 paru -S --noconfirm zramd google-chrome xdman
 sudo
 
-sudo pacman -S --noconfirm xorg sddm plasma firefox vlc alacritty audacity kdeconnect ark
+sudo pacman -S --noconfirm xorg sddm plasma firefox vlc alacritty audacity kdeconnect ark phonon-qt5 qt5-webkit qt5-script qt5-svg qt5-tools qt5-x11extras enchant jasper openexr libutempter docbook-xsl shared-mime-info giflib libxss upower udisks2 bzr git doxygen perl-json perl-libwww perl-xml-parser perl-io-socket-ssl akonadi xorg-server-devel libpwquality fontforge eigen libfakekey qca-qt5 xapian-core xsd gperf perl-yaml-syck intltool kdesdk qrencode libdmtx boost ruby-test-unit
 
 sudo flatpak install -y spotify
 
