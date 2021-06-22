79d46a52942f8d1ebeaf5b37ff064db249b51f30
Debarka Mondal <37607719+debarkamondal@users.noreply.github.com>
2021-06-22 11:12:47 +0530

    Update xorg.sh

--- xorg.sh
+++ xorg.sh
@@ -12,11 +12,10 @@ makepkg -si --noconfirm
 paru -S --noconfirm zramd xdman brave picom-jonaburg-git
 sudo systemctl enable --now zramd.service
 
-sudo pacman -S --noconfirm xorg xorg-init vlc alacritty audacity 
+sudo pacman -S --noconfirm xorg xorg-init vlc alacritty audacity python-psutils
 sudo systemctl enable zram 
 sudo flatpak install -y spotify
 
-sudo systemctl enable sddm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 reboot
