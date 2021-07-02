978eae1097529dcec4043e3dbc4fa3688bc86c99
Debarka Mondal <37607719+debarkamondal@users.noreply.github.com>
2021-07-02 22:34:38 +0530

    Update xorg.sh

--- xorg.sh
+++ xorg.sh
@@ -9,13 +9,10 @@ git clone https://aur.archlinux.org/paru.git
 cd paru/
 makepkg -si --noconfirm
 
-paru -S --noconfirm zramd xdman brave picom-jonaburg-git  nerd-fonts-fira-code nerd-font-hack starship python-psutils
+paru -S --noconfirm zramd xdman brave picom-jonaburg-git nerd-fonts-fira-code nerd-font-hack starship python-psutils
 sudo systemctl enable --now zramd.service
 
-sudo pacman -S --noconfirm xorg xorg-init vlc alacritty audacity fish lxqt-policykit
+sudo pacman -S --noconfirm xorg xorg-init vlc alacritty audacity lxqt-policykit zsh zsh-autocompletion zsh-syntax-highlighting zsh-autosuggestions lm_sensors
 sudo systemctl enable zram 
-sudo flatpak install -y spotify
 
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-reboot
+echo "Please reboot"
