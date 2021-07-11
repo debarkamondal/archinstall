28aa8e55b7e9a5168ebfe9ac6cce42c2f15dd43e
Debarka Mondal <37607719+debarkamondal@users.noreply.github.com>
2021-07-11 17:21:19 +0530

    Update qtile.sh

--- qtile.sh
+++ qtile.sh
@@ -9,9 +9,9 @@ git clone https://aur.archlinux.org/paru.git
 cd paru/
 makepkg -si --noconfirm
 
-paru -S --noconfirm zramd xdman brave picom-jonaburg-git nerd-fonts-fira-code nerd-font-hack starship timeshift timeshift-autosnap
-echo "MAX_SIZE=2048"
+paru -S --noconfirm zramd xdman brave-bin picom-jonaburg-git nerd-fonts-fira-code nerd-font-hack starship timeshift timeshift-autosnap xwinwrap
+echo "MAX_SIZE=2048" >> /etc/default/zramd
 sudo systemctl enable --now zramd.service
 
-sudo pacman -S --noconfirm qtile xorg xorg-init vlc alacritty audacity lxqt-policykit zsh zsh-autocompletion zsh-syntax-highlighting python-psutils zsh-autosuggestions lm_sensors
+sudo pacman -S --noconfirm qtile xorg xorg-xinit vlc alacritty lxqt-policykit zsh zsh-syntax-highlighting python-psutil zsh-autosuggestions lm_sensors mpv file-roller thunar thunar-archive-plugin thunar-volman
 echo "Please reboot"
